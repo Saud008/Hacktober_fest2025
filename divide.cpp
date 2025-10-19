d5534400f45175d07c8892bcebf8fd826e8d8888
Saud masaud alam <127945508+Saud008@users.noreply.github.com>
2025-10-19 15:28:13 +0530

    Create divide.cpp

---
+++ divide.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_set<int> set;
+        for(int num : nums) {
+            if(set.count(num)) {
+                set.erase(num);
+            } else {
+                set.insert(num);
+            }
+        }
+        return set.empty();
+    }
+};
